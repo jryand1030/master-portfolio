185f8316bdac8601a2fd0bb432b6541fb848f895
Ryan Dionne <jryand1030@gmail.com>
2018-10-22 01:19:37 -0600

    added portfolio-and-contact

--- js/main.js
+++ js/main.js
@@ -10,3 +10,16 @@ var nameanimation = bodymovin.loadAnimation({
 $(document).ready(function(){
     $(".btn").append('<span></span><span></span>');
 });
+
+
+
+$(document).on("scroll", function(){
+    if
+  ($(document).scrollTop() > 86){
+      $("#banner").addClass("shrink");
+    }
+    else
+    {
+        $("#banner").removeClass("shrink");
+    }
+});
